da69f184063acf4137b3a9574696f26180a932d7
Jie XU <jie.xu.xj@gmail.com>
2015-08-26 09:05:45 -0500

    Create Sex.java

---
+++ Sex.java
@@ -0,0 +1,6 @@
+public enum Sex {
+	NONE{public String getName(){return "男女不限";} },
+	MAN{public String getName(){return "男";} },
+	WOMEN{public String getName(){return "女";}};
+	public abstract String getName();
+}
